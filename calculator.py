45a6b356bcc7cdb5c5760c88f38c9e3d23fa7785
theboxboy1 <126028130+theboxboy1@users.noreply.github.com>
2024-10-16 17:35:35 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ def circle(r):
     
     return PI*r**2
 
-# To import this function in an other script, simply call the module by using "import *filename without .py*"
-# Ex.  import calculator
+# To import this function in an other script, simply call the module by using from and import the function by using import
+# Ex.  from calculator import circle
 # To use the module, call it's function "circle", which takes one parameter
 #Ex. circle(int(input())) will call the function and pass on a user inputted integer to calculate the area of a circle
